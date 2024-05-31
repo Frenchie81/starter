b3ff333f7c24b1a6c6f9b4b5f4770a084302753f
Brian French <bfrench9481@gmail.com>
2024-05-31 08:37:46 -0400

    Fix dadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,5 +1,15 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+  {
+    "tpope/vim-dadbod",
+  },
+  -- "kristijanhusak/vim-dadbod-completion",
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = { "tpope/vim-dadbod" },
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    dependencies = { "tpope/vim-dadbod", "kristijanhusak/vim-dadbod-ui" },
+    ft = "sql",
+  },
 }
