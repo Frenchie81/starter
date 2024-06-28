988d647adc6de7b90c483b700caadbabc62e352d
Brian French <bfrench9481@gmail.com>
2024-06-28 15:54:45 -0400

    switch back to tokyo

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,27 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      colorscheme = "carbonfox",
-    },
-    lazy = false,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          dim_inactive = true,
-          styles = {
-            comments = "italic",
-          },
-        },
-      })
-      vim.cmd("colorscheme carbonfox")
-    end,
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
   },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   opts = {
+  --     colorscheme = "carbonfox",
+  --   },
+  --   lazy = false,
+  --   config = function()
+  --     require("nightfox").setup({
+  --       options = {
+  --         dim_inactive = true,
+  --         styles = {
+  --           comments = "italic",
+  --         },
+  --       },
+  --     })
+  --     vim.cmd("colorscheme carbonfox")
+  --   end,
+  -- },
   -- {
   --   "projekt0n/github-nvim-theme",
   --   lazy = true,
