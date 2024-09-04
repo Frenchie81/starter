923017c7b10ccfeea9d0096f8f48cf402f670810
Brian French <bfrench9481@gmail.com>
2024-09-03 23:35:30 -0400

    Disable lorem because it has an error at start

--- lua/plugins/lorem.lua
+++ lua/plugins/lorem.lua
@@ -1,10 +1,11 @@
-return {
-  "derektata/lorem.nvim",
-  config = function()
-    require("lorem").setup({
-      sentenceLength = "medium",
-      comma_chance = 0.2,
-      max_commas_per_sentence = 2,
-    })
-  end,
-}
+return {}
+-- return {
+--   "derektata/lorem.nvim",
+--   config = function()
+--     require("lorem").setup({
+--       sentenceLength = "medium",
+--       comma_chance = 0.2,
+--       max_commas_per_sentence = 2,
+--     })
+--   end,
+-- }
