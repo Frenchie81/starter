debbcf1d9230bd36fbd7fd5e9d0b5785115073d2
Brian French <bfrench9481@gmail.com>
2024-09-23 16:32:57 -0400

    add rust test support

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -2,6 +2,6 @@ return {
   { "Issafalcon/neotest-dotnet" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-dotnet" } },
+    opts = { adapters = { "neotest-dotnet", "rustaceanvim.neotest" } },
   },
 }
