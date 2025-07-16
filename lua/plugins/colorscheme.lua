b947bbd352300a3eb47d0a56eebdbb40088216d2
Brian French <bfrench9481@gmail.com>
2025-07-16 10:25:51 -0400

    try the vscode dark modern theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "gmr458/vscode_modern_theme.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vscode_modern").setup({
+        cursorline = true,
+        transparent_background = false,
+        nvim_tree_darker = true,
+      })
+    end,
+  },
+  {
     "projekt0n/github-nvim-theme",
     lazy = true,
     config = function()
@@ -16,7 +28,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_dimmed",
+      colorscheme = "vscode_modern",
     },
   },
 }
