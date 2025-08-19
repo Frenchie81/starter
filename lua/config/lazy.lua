a4d7a7b279fb2e9964b421ebad98c3c84d6cfc5c
Brian French <bfrench9481@gmail.com>
2025-08-19 09:57:29 -0400

    remove solution wide analysis

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,12 +52,3 @@ require("lazy").setup({
     },
   },
 })
-
-vim.lsp.config("roslyn", {
-  settings = {
-    ["csharp|background_analysis"] = {
-      dotnet_analyzer_diagnostics_scope = "fullSolution",
-      dotnet_compiler_diagnostics_scope = "fullSolution",
-    },
-  },
-})
